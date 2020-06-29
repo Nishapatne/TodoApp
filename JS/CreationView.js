72d2caf1336520ea169986768a08b2084856b095
Nisha Patne <nishabaitule@hotmail.com>
2020-06-29 22:35:57 +0530

    added validation and alignment of page

--- JS/CreationView.js
+++ JS/CreationView.js
@@ -1,18 +1,23 @@
-var itemCount = 1
+var itemCount = 1;
+var shouldAddItem = true;
 function addNewItem(){
    
+    if(!addItemValidation()){
+        return;
+    }
     var outerDiv = document.getElementById("outerDiv")
     var newDiv = document.createElement("div");
 
     var newCheckbox = document.createElement("input");
     newCheckbox.type = "checkbox";
-    newCheckbox.id = itemCount;   
+    newCheckbox.id = itemCount;  
     newCheckbox.addEventListener("click" ,checkboxListner);
     
     var newTextBox = document.createElement("input");
     newTextBox.type = "textbox"; 
-    newTextBox.id = "text"+itemCount;
+    newTextBox.id = "textbox"+itemCount;
     newTextBox.className = "textbox"
+    newTextBox.addEventListener("change", textboxChangecolor)
    
 
     var trashImg = document.createElement("IMG");
@@ -24,22 +29,20 @@ function addNewItem(){
 
     itemCount++;
 
-
     var brk = document.createElement("br");
     newDiv.appendChild(brk);
 
     newDiv.appendChild(newCheckbox);
     newDiv.appendChild(newTextBox);
     newDiv.appendChild(trashImg);
-    outerDiv.appendChild(newDiv);  
-
+    outerDiv.appendChild(newDiv);
 }
 
 
 function checkboxListner(){
     
     var checkBox = document.getElementById(this.id);
-    var textBox = document.getElementById("text" + this.id);
+    var textBox = document.getElementById("textbox" + this.id);
 
     if(checkBox.checked == true){
         textBox.style.textDecoration = "line-through";
@@ -58,6 +61,23 @@ function submit(){
     alert("Your Task List Created!");
 }
 
+function addItemValidation(){
+    for(i = 1;i<itemCount;i++){
+        var elem = document.getElementById("textbox" + i);
+        
+        if(elem!=null && elem.value==""){
+            alert("Item can not be empty");
+            elem.style.border = "1px solid red";
+            return false;
+        }
+    }    
+    return true;   
+}
+
+function textboxChangecolor(){
+    var eltem = document.getElementById(this.id);
+    eltem.style.border = "1px solid black";
+}
 
 
 
