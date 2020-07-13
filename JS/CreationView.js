842394b3b570be687695f8db82382c663ffa1881
nishabaitule <64946102+nishabaitule@users.noreply.github.com>
2020-07-13 19:37:50 +0530

    Missed Changes for implemenation change JavaScript with JSON array for Input Template

--- JS/CreationView.js
+++ JS/CreationView.js
@@ -3,6 +3,8 @@ var listCount = 1;
 var shouldAddItem = true;
 
 var todoListArray = [];
+var todoListjsonData = {};
+todoListjsonData["lists"] = [];
 
 function addNewItem(){
    
@@ -20,9 +22,9 @@ function addNewItem(){
     $(newCheckbox).on('click', function() {
         checkboxevent(this);
     });
-    
+   
     var newTextBox = document.createElement("input");
-    newTextBox.type = "textbox"; 
+    newTextBox.type = "textbox";
     $(newTextBox).addClass("textbox");
     $(newTextBox).attr("id","textbox"+     itemCount);
     $(newTextBox).on('change', function(){
@@ -38,6 +40,7 @@ function addNewItem(){
         $(trashImg).on('click', function(){
             removediv(this);
         })
+
     itemCount++;
 
     var brk = document.createElement("br");
@@ -55,7 +58,7 @@ function checkboxevent(elem){
     var newtextBox = $('#'+"textbox"+id);
     if(newcheckBox.is(":checked") == true){
          $(newtextBox).addClass("checkBoxSelected");
-        
+       
     }
     else{
         $(newtextBox).addClass("checkBoxUnSelected");
@@ -83,11 +86,13 @@ function submit(){
 
     // todoList.title = title
     todoList.setTitle(title);
+
+    var list = {"title":title,"items":[]};    
     todoList.setStatus("Active");
 
     for(i = 1;i<itemCount;i++){
         var elem = $("#textbox" + i);
-        
+       
         if(elem!=null){
             var item = new Item("itemId"+i);
             var checkBox = $(i);
@@ -100,17 +105,19 @@ function submit(){
             }
 
             item.setTitle(elem.val());
-            
-
+            list["items"].push({"item":elem.val()});
+           
            todoList.addItem(item);
            elem.val("");
         }
-    } 
+    }
 
+    todoListjsonData["lists"].push(list);
     todoListArray.push(todoList);
 
+
     // For Displaying lists
-    var todotitleListsHtmlComponent = rendertodoList(todoListArray);
+    var todotitleListsHtmlComponent = rendertodoListUsingTemplate(todoListArray);
     document.getElementById("todoList").innerHTML = '';
     // $("todoList").val('');
     document.getElementById("todoList").append(todotitleListsHtmlComponent);
@@ -128,6 +135,20 @@ function rendertodoList(todoListArray) {
     return todotitleListsHtmlComponent;
 }
 
+function rendertodoListUsingTemplate() {
+
+  var template = document.getElementById('template').innerHTML;
+
+  //Compile the template
+  var compiled_template_funtion = Handlebars.compile(template);
+
+  //Render the data into the template
+  var htmlDynamicContent = compiled_template_funtion(todoListjsonData);
+
+  //Overwrite the contents of #target with the renderer HTML
+  document.getElementById('target').innerHTML = htmlDynamicContent;
+}
+
 function createListComponent(todoList) {
 
     var listDiv = document.createElement('div');
@@ -136,13 +157,13 @@ function createListComponent(todoList) {
     listTitle.appendChild(title);  // Append Title to header
     listDiv.appendChild(listTitle);  //Append header to list div
 
-    
+   
     var itemsList = document.createElement('ul'); //create list for items
     var list = todoList.getItemList();
     list.forEach(item => {
 
         var itemHtmlElement = document.createElement('li');
-        //@nisha try to create seperate column for status 
+        //@nisha try to create seperate column for status
         itemHtmlElement.appendChild(document.createTextNode("( Status:"+item.getStatus()+") "+ item.getTitle()));
         itemsList.appendChild(itemHtmlElement);
     });
@@ -155,14 +176,14 @@ function createListComponent(todoList) {
 function addItemValidation(){
     for(i = 1;i<itemCount;i++){
         var elem = $("#textbox" + i);
-        
+       
         if(elem!=null && elem.val()==""){
             alert("Item can not be empty");
             elem.style.border = "1px solid red";
             return false;
         }
     }    
-    return true;   
+    return true;  
 }
 
     let design = {
@@ -229,7 +250,7 @@ class TodoList {
 
     getStatus(){
         return this.status;
-    } 
+    }
 
     getItemList(){
         return this.itemList;
