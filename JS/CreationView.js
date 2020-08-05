32dab5a1e484a5d94e3c3cf47039ae2c39394ccb
nishabaitule <64946102+nishabaitule@users.noreply.github.com>
2020-08-05 10:03:21 +0530

    css changes

--- JS/CreationView.js
+++ JS/CreationView.js
@@ -8,85 +8,95 @@ todoListjsonData["lists"] = [];
 
 function addNewItem(){
    
-    if(!addItemValidation()){
+    if(!addItemValidation())
+    {
         return;
     }
     // var outerDiv = document.getElementById("outerDiv")
     var outerdiv = $('#outerdiv');
     var newDiv = document.createElement("div");
     $(newDiv).addClass("newdivision");
+    $(newDiv).attr("id","newdivision"+ itemCount);
 
+    var newlabel = document.createElement("label");
+    $(newlabel).attr("id","newlabel"+ itemCount);
+    var indexCurrent = itemCount;
     var newCheckbox = document.createElement("input");
     newCheckbox.type = "checkbox";
     $(newCheckbox).addClass("checkbox");
-    $(newCheckbox).attr("id", itemCount);
-    $(newCheckbox).on('click', function() {
-        checkboxevent(this);
-    });
+    $(newCheckbox).attr("id","checkbox"+ itemCount);
+    $(newCheckbox).on('click', function() 
+        {
+            checkboxevent(this,indexCurrent);
+        }
+    );
 
-
-    if(isInEditMode){
-
-    var newTextBox = document.createElement("input");
-    newTextBox.type = "textbox";
-    $(newTextBox).addClass("textbox");
-    $(newTextBox).attr("id","textbox"+ itemCount);
-    $(newTextBox).on('change', function(){
-        textboxchangecolor(this);
-    })
+    if(isInEditMode)
+    {
+        var newTextBox = document.createElement("input");
+        newTextBox.type = "textbox";
+        $(newTextBox).addClass("textbox");
+        $(newTextBox).attr("id","textbox"+ itemCount);
+        $(newTextBox).on('change', function(){
+        textboxchangecolor(this)})
     }
-    else{
+
+    else
+    {
         // var textbox = document.createElement("label")
-    var newTextBox =  document.createElement("Label");
-    newTextBox.setAttribute("for",itemCount);
-    newTextBox.innerHTML = "label Mode";
-    $(newTextBox).addClass("labeltext");    
+        var newTextBox =  document.createElement("Label");
+        newTextBox.setAttribute("for",itemCount);
+        newTextBox.innerHTML = "label Mode";
+        $(newTextBox).addClass("labeltext");    
     }
-
+    
     var trashImg = document.createElement("IMG");
     trashImg.setAttribute("src", "../Images/delete.png");
     trashImg.type = "IMG";
     $(trashImg).addClass("delImg")
-        $(trashImg).attr("id", itemCount);
-        $(trashImg).on('click', function(){
+    $(trashImg).attr("id","delImg"+itemCount);
+    $(trashImg).on('click', function()
+        {
             removediv(this);
-        })
+        }
+    )
 
     itemCount++;
+    newlabel.appendChild(newCheckbox);
+    newlabel.appendChild(newTextBox);
+    newlabel.appendChild(trashImg);
 
-    // var brk = document.createElement("br");
-    // newDiv.appendChild(brk);
-
-    newDiv.appendChild(newCheckbox);
-    newDiv.appendChild(newTextBox);
-    newDiv.appendChild(trashImg);
+    newDiv.appendChild(newlabel);
     outerDiv.appendChild(newDiv);
 }
 
-function checkboxevent(elem){
-    var id = $(elem).attr("id");
+
+function checkboxevent(elem,indexCurrent)
+{
+    // var id = $(elem).attr("id");
     // var newcheckBox = $('#'+id);
-    var newtextBox = $('#'+"textbox"+id);
-   
-    $(newtextBox).toggleClass("checkBoxaction");   
-   
+    var newtextBox = $("#textbox"+indexCurrent);
+    $(newtextBox).toggleClass("checkBoxaction");     
 }
 
-function removediv(elem){
+function removediv(elem)
+{
     elem.parentNode.parentNode.removeChild(elem.parentNode);
 }
 
-function submit(){
+function submit()
+{
     var todoList = new TodoList(listCount++);  
     // var title =  document.getElementById("todoListTitle").value;
     var title =  $("#todoListTitle").val();
 
-    if(title == ""){
-        //add alert box
+    if(title == "")
+    {
         alert("Title can not be empty");
         return;
     }
-    else{
+    else
+    {
         $("#todoListTitle").val("");
     }
 
@@ -100,15 +110,18 @@ function submit(){
         var elem1 = $("#" +i);
         var elem = $("#textbox" + i);
        
-        if(elem!=null){
+        if(elem!=null)
+        {
             var item = new Item("itemId"+ i);
             var checkBox = $("#"+i);
 
-            if(checkBox.is (":checked")){
+            if(checkBox.is (":checked"))
+            {
             // if(    $('#' + i).is(":checked")){ 
             item.setStatus("Completed");
             }
-            else{
+            else
+            {
                 item.setStatus("Pending");
             }
 
@@ -120,6 +133,8 @@ function submit(){
            todoList.addItem(item);
            elem1. prop("checked", false);
            elem.val("");
+            //removediv($("#newdivision" + i));
+            removediv(document.getElementById('newlabel'+i));
         }
     }
 
@@ -131,18 +146,17 @@ function submit(){
     // document.getElementById("todoList").innerHTML = '';
     $("#todoList").val('');
     // document.getElementById("todoList").append(todotitleListsHtmlComponent);
-    $("#todoList").append(todotitleListsHtmlComponent)
+    $("#todoList").append(todotitleListsHtmlComponent);
+    itemCount = 1;
 }
 
-function rendertodoList(todoListArray) {
-
+function rendertodoList(todoListArray) 
+{
     var todotitleListsHtmlComponent= document.createElement('div');
-
     todoListArray.forEach(todoList => {
         var todoitemListHtmlComponent= createListComponent(todoList)
         todotitleListsHtmlComponent.appendChild(todoitemListHtmlComponent);
     });
-
     return todotitleListsHtmlComponent;
 }
 
@@ -184,91 +198,112 @@ function createListComponent(todoList) {
     return listDiv;
 }
 
-function addItemValidation(){
-    for(i = 1;i<itemCount;i++){
+function addItemValidation()
+{
+    for(i = 1;i<itemCount;i++)
+    {
         var elem = $("#textbox" + i);
        
-        if(elem!=null && elem.val()==""){
+        if(elem!=null && elem.val()=="")
+        {
             alert("Item can not be empty");
             $(elem).addClass("textboxbordercolor") ;
             return false;
                 
         }
-            else{
-                $(elem).addClass("textboxbordercolorNew") ;
-            }
+        else
+        {
+            $(elem).addClass("textboxbordercolorNew") ;
+        }
     }    
     return true;  
 }
-    // $(newtextBox).toggleClass("checkBoxaction");  
 
-    // let design = {
-    //     border: "1px solid black"
-    // }
-function textboxchangecolor(elem){
+// $(newtextBox).toggleClass("checkBoxaction");  
+// let design = {
+//     border: "1px solid black"
+// }
+
+function textboxchangecolor(elem)
+{
     var id = $(elem).attr("id");
     var textboxcolor = $('#' + id);
     $(textboxcolor).addClass("headboxbordercolor");
 }
 
-class Item {
-    constructor(id) {
+class Item 
+{
+    constructor(id) 
+    {
       this.id = id;
       this.itemTitle = "";
       this.status = "Pending";
     }
 
-    setTitle(title){
+    setTitle(title)
+    {
         this.itemTitle = title;
     }
 
-    setStatus(status){
+    setStatus(status)
+    {
         this.status = status;
     }
 
-    getTitle(){
+    getTitle()
+    {
        return  this.itemTitle;
     }
 
-    getStatus(){
+    getStatus()
+    {
         return this.status;
     }
 }
 
 
-class TodoList {
-    constructor(id) {
+class TodoList 
+{
+    constructor(id) 
+    {
       this.id = id;
       this.listTitle = "";
       this.status = "Active";
       this.itemList = [];
     }
 
-    setTitle(title){
+    setTitle(title)
+    {
         this.itemTitle = title;
     }
 
-    setStatus(status){
+    setStatus(status)
+    {
         this.status = status;
     }
 
-    setItemList(list){
+    setItemList(list)
+    {
         this.ItemList  = list;
     }
 
-    addItem(item){
+    addItem(item)
+    {
         this.itemList.push(item);
     }
 
-    getTitle(){
+    getTitle()
+    {
         return this.itemTitle;
     }
 
-    getStatus(){
+    getStatus()
+    {
         return this.status;
     }
 
-    getItemList(){
+    getItemList()
+    {
         return this.itemList;
     }
 }
